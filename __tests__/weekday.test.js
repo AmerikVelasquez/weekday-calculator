7a60b66e465269bead2dfcc1b9178209cc62597c
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-07-27 20:57:37 -0700

    add initial code for a good fail
    Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>

---
+++ __tests__/weekday.test.js
@@ -0,0 +1,9 @@
+import weekdayCalculator from './../src/weekday.js'
+
+describe('weekday', () => {
+
+  test('it should recive a date string and return the day of the week that corresponds',() =>{
+    weekdayCalculator("05,22,1997");
+     expect() 
+  })
+})
