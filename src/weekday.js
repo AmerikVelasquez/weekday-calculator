bec88165f247f5ff3f9a0a39d731bd86ebe775b0
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-07-28 18:55:18 -0700

    add good pass test to weekay calculator
    Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>

--- src/weekday.js
+++ src/weekday.js
@@ -1,3 +1,5 @@
 export const weekdayCalculator = (dateString) => {
-
+  const array= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const date = new Date(dateString);
+  return array[date.getDay()];
 };
