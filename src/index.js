45ea5be27db9f55d832d1ee06e11e155d563e29f
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-07-28 20:22:30 -0700

    add user interface logic
    Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import { weekdayCalculator } from './weekday.js';
+
+$("#formOne").submit(function(event){
+  event.preventDefault();
+  const userDate = $("#userDate").val();
+  const htmlForResults = `<h3>The day of the week is <span id = 'dayOutput'><span>.</h3>`;
+  $("#results").show();
+  $("#results").html(htmlForResults);
+  $("#dayOutput").text(weekdayCalculator(userDate));
+});
